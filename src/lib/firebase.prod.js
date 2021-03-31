c980f71f3160acddacc934bc2ef58251f157536c
boa <boa.matule@gmail.com>
2021-03-31 04:14:57 +0200

    Creates a seed.js and populates it with data for the firestore; Creates a Context folder and inside it, creates a fibare.js for the createContext; Imports the FirebaseContext to the index.js in the src folder and wraps the render;

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,9 +1,17 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { seedDatabase } from '../seed';
 
 
-const config = {};
+const config = {
+	apiKey: "AIzaSyB-BYsLba9iqyn-me7U5GDUDCgh0VloGDg",
+	authDomain: "netflix-f8abd.firebaseapp.com",
+	projectId: "netflix-f8abd",
+	storageBucket: "netflix-f8abd.appspot.com",
+	messagingSenderId: "1033974975835",
+	appId: "1:1033974975835:web:0b9988de1cb08e5dc11e7e"
+};
 
 
 const firebase = Firebase.initializeApp(config);
