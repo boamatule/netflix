920da1b0abf527212ad7e9059fc8cbf01ca39ee6
boa <boa.matule@gmail.com>
2021-04-01 17:25:00 +0200

    Creates a ProtectedRoute and wraps the Browse in the App.

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ export default function App() {
         >
           <Signup />
         </IsUserRedirect>
+        
         <ProtectedRoute user={user} path={ROUTES.BROWSE}>
           <Browse />
         </ProtectedRoute>
