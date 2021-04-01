7e57736d524ec81558198f960483c803bd75a238
boa <boa.matule@gmail.com>
2021-04-01 03:35:47 +0200

    Imports FirebaseContext and deconstruction it; creates a boilerplate for the firebase; imports useHistory and ROUTES to be used to send the user to the app after the login...WIP

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,10 +1,15 @@
 import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
 import { FooterContainer } from '../containers/footer';
 import { HeaderContainer } from '../containers/header';
 import { Form } from '../components';
+import * as ROUTES from '../constants/routes';
 
 
 export default function Signin() {
+	const history = useHistory();
+	const { firebase } = useContext(FirebaseContext);
 	const [emailAddress, setEmailAddress] = useState('');
 	const [password, setPassword] = useState('');
 	const [error, setError] = useState('');
@@ -12,7 +17,20 @@ export default function Signin() {
 	const isInvalid = password === '' || emailAddress === '';
 	const handleSignIn = (event) => {
 		event.preventDefault();
+
+		firebase
+		.auth()
+		.signInWithEmailAndPassword(emailAddress, password)
+		.then(() => {
+			history.piush(ROUTES.BROWSE);
+		})
+		.catch((error) => {
+			setEmailAddress('');
+			setPassword('');
+			setError(error.message);
+		})
 	};
+
 
 	return ( 
 		<>
