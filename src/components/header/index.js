7008ec0a8396d309cd74456772c84acd2c619a83
boa <boa.matule@gmail.com>
2021-04-03 03:21:52 +0200

    Creates a Profiles.js in the containers folder and imports Header from the component and ROUTEs and display the logo in the created function SelectProfileContainer; Export the Profiles.js to the browse. WIP.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link as ReactRouterLink } from 'react-router-dom';
-import { Container, Logo, Background, ButtonLink, Feature } from './styles/header';
+import { Container, Logo, Background, ButtonLink } from './styles/header';
 
 export default function Header ({ bg= true, children, ...restProps }) {
 	return bg ? <Background { ...restProps}>{children}</Background> : children;
@@ -10,9 +10,6 @@ Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
 	return <ButtonLink { ...restProps}>{children}</ButtonLink>
 };
 
-// Header.Feature = function HeaderFeature({ children, ...restProps }) {
-//   return <Feature>{children}</Feature>;
-// };
 
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
 	return <Container { ...restProps}>{children}</Container>
---
+++ src/containers/browse.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SelectProfileContainer } from './profiles';
+
+export function BrowseContainer({ slides }) {
+	return (
+		<SelectProfileContainer />
+	)
+}
